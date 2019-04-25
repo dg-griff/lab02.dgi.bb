08af6f6c380fdfb709b4560a6f43be9214d5b257
dg-griff <dg4.griff@gmail.com>
2019-04-24 22:13:21 -0400

    Implemented Javascript to satisfy reqs for Lab 02

--- js/app.js
+++ js/app.js
@@ -1,21 +1,55 @@
 'use strict';
 
-var questions = ['Is Suzuki my favourite car manufactuer?', 'Is Android my favourite mobile OS?', ' Is Dancehall my favourite music genre?', 'Is Black my least favourite colour?', 'Do I seldom eat at Subway restaurants?' ];
+// Prompt user for name
+var user = prompt('Hello and welcome to the DGi® Guessing Game! What is your name, please?').toUpperCase();
 
-var replies = ['no', 'yes', 'yes', 'no', 'no'];
+// Greet User
+alert('Pleasure to meet you, ' + user + '! Let\'s begin! I will ask you five questions about me and you are to respond with yes OR no.');
 
-//Prompt user for their name
-var user = prompt('Hi there, what is your name?');
-//Greet user
-alert("Hi " + user + " !");
-
-for (var i = 0; i < questions.length; i++) {
-  var response = prompt(questions[i]).toLowerCase();
-  if (response === replies[i]) {
-    alert('You are correct!');
-    console.log(response + ' was the correct answer to the question \"' + questions[i] + '\"');
-  } else {
-    alert('You are wrong!');
-    console.log(response + ' was the incorrect answer to the question \"' + questions[i] + '\"');
-  }
+// Ask Five Questions
+var response = prompt('Is Suzuki my favourite car manufacturer?').toLowerCase();
+if((response === 'no') || (response === 'n')) {
+  alert('You are correct! Well done.')
+  console.log(response + ' was the correct answer to the question.');
+} else {
+  alert('you are incorrect! Are you sure that you are taking this seriously?!');
+  console.log(response + ' was the incorrect answer to the question.');
 }
+
+var response_2 = prompt('Is Android my favourite mobile Operating System?').toLowerCase();
+if((response_2 === 'yes') || (response_2 === 'y')) {
+  alert('You are correct! Well done.')
+  console.log(response_2 + ' was the correct answer to the question.');
+} else {
+  alert('you are incorrect! Are you sure that you are taking this seriously?!');
+  console.log(response_2 + ' was the incorrect answer to the question.');
+}
+
+var response_3 = prompt('Is Dancehall my favourite genre of music?').toLowerCase();
+if((response_3 === 'yes') || (response_3 === 'y')) {
+  alert('You are correct! Well done.')
+  console.log(response_3 + ' was the correct answer to the question.');
+} else {
+  alert('you are incorrect! Are you sure that you are taking this seriously?!');
+  console.log(response_3 + ' was the incorrect answer to the question.');
+}
+
+var response_4 = prompt('Is Black my least favourite colour to wear?').toLowerCase();
+if((response_4 === 'no') || (response_4 === 'n')) {
+  alert('You are correct! Well done.')
+  console.log(response_4 + ' was the correct answer to the question.');
+} else {
+  alert('you are incorrect! Are you sure that you are taking this seriously?!');
+  console.log(response_4 + ' was the incorrect answer to the question.');
+}
+
+var response_5 = prompt('Is Subway the restaurant I eat at least often?').toLowerCase();
+if((response_5 === 'no') || (response_5 === 'n')) {
+  alert('You are correct! Well done.')
+  console.log(response_5 + ' was the correct answer to the question.');
+} else {
+  alert('you are incorrect! Are you sure that you are taking this seriously?!');
+  console.log(response_5 + ' was the incorrect answer to the question.');
+}
+
+alert('Game Over! Thanks for playing!');
