e54d1a3e68cd6cf870aeb6462655c28e84e2b0b7
dg-griff <dg4.griff@gmail.com>
2019-04-26 09:12:14 -0400

    Implemented Javascript for 6th and 7th questions on Lab 03 Game

--- js/app.js
+++ js/app.js
@@ -1,16 +1,29 @@
 'use strict';
 
+// Variables for questions 1-5
 var questions = ['Is Suzuki my favourite car manufactuer?', 'Is Android my favourite mobile OS?', ' Is Dancehall my favourite music genre?', 'Is Black my least favourite colour?', 'Do I seldom eat at Subway restaurants?' ];
 var replies = ['no', 'yes', 'yes', 'no', 'no'];
 var replies_2 = ['n', 'y', 'y', 'n', 'n'];
 var score = 0;
 
+// Variables for question 6
+var num = 150;
+var guess;
+var n = 4;
+
+// Variables for 7th question
+var options = ['platz', 'corolla', 'auris'];
+var s = 6;
+var c = 0;
+
+// Questions 1-5
 //Prompt user for their name
 var user = prompt('Hello and welcome to the DGi® Guessing Game! Can you tell me your name, please?').toUpperCase();
 //Greet user
-alert("Pleasure to meet you, " + user + "! Let\'s begin! I will ask you five questions about myself and you are to respond with either yes OR no.");
+alert("Pleasure to meet you, " + user + "! Let\'s begin! I will ask you five questions about myself. To respond, enter \"y or yes\" OR \"n or no\".");
 
 for (var i = 0; i < questions.length; i++) {
+  alert('Question ' + (i + 1) + ':');
   var response = prompt(questions[i]).toLowerCase();
   if ((response === replies[i]) || (response === replies_2[i])) {
     alert('You are correct! Well done.');
@@ -22,86 +35,62 @@ for (var i = 0; i < questions.length; i++) {
   }
 }
 
+// Question 6
+alert('Question ' + (i + 1) + ':');
+var num_guess = prompt('I am an old school Pokémon player. Can you guess how many Pokémon I have caught so far?');
+if (num_guess == 150) {
+  score++;
+  alert('Congrats! You got it correct with only one guess!'); 
+} else {
+  while ((num_guess != 150) && (n > 1)) {
+      n--;
+      var guess = prompt('You have ' + n + ' more attempt(s) to get it correct! Guess again!');
+      if (guess > num) {
+          alert('Whoa! Your guess was too high! I have not caught that many Pokémon!');
+      } else if (guess < num) {
+          alert('Whoa! Your guess was too low! I have caught more Pokémon than that!');
+      } else {
+          if (guess > num) {
+              alert('Whoa! Your guess was too high! I have not caught that many Pokémon!');
+            } else if (guess < num) {
+            alert('Whoa! Your guess was too low! I have caught more Pokémon than that!');
+            } else {
+              alert('You are absolutely correct! Well done!');
+              score++;
+              break;
+          }
+      }  
+  }
+  alert('You have reached the maximum number of 4 incorrect attempts! Game Over!');    
+}
+
+// Question 7
+alert('Question ' + (i + 2) + ':');
+var reply = prompt('Toyota has manufactured many different car models over the years. Three of these models are my favourites. Can you guess one of them?').toLowerCase();
+
+if (options.indexOf(reply) !== -1) {
+    alert('You got it on the first try! Amazing job!');
+    c++;
+    score++;
+} else {
+    while ((options.indexOf(reply) === -1) && (s > 1)) {
+        s--;
+        var retry = prompt('You have ' + s + ' more attempt(s) to get it correct! Guess again!');
+        if (options.indexOf(retry) !== -1) {
+            alert('You finally got it! Good job');
+            c++;
+            break;
+        } else {
+            alert('You are still wrong!');
+        }
+    }
+}
+
+if(c > 0) {
+    alert('Congrats on completing the game! Proceed to next screen for your final score.');
+} else {
+    alert('You have reached the maximum of 6 incorrect attempts! Proceed to next screen for your final score.');
+}
+
+
 alert('Game Over! ' + user + ', your final score is: ' + score + ', meaning you got ' + score + ' out of 7 questions correct! Thanks for playing!');
-
-
-// // Question 6 variables
-// // var num = 150;
-// // var num_guess = prompt('I am an old school Pokémon player. Can you guess how many Pokémon I have caught so far?');
-// // var guess;
-// // var n = 4;
-
-// // Question 7 variables
-// var options = ['platz', 'yaris', 'corolla'];
-// var s = 6;
-
-// // if (num_guess == 150) {
-// //     alert('Congrats! You got it correct with only one guess!');
-// // } else {
-// //     while ((num_guess != 150) && (n > 1)) {
-// //         n--;
-// //         var guess = prompt('You have ' + n + ' more attempt(s) to get it correct! Guess again!');
-// //         if (guess > num) {
-// //             alert('Whoa! Your guess was too high! I have not caught that many Pokémon!');
-// //         } else if (guess < num) {
-// //             alert('Whoa! Your guess was too low! I have caught more Pokémon than that!');
-// //         } else {
-// //             if (guess > num) {
-// //                 alert('Whoa! Your guess was too high! I have not caught that many Pokémon!');
-// //               } else if (guess < num) {
-// //               alert('Whoa! Your guess was too low! I have caught more Pokémon than that!');
-// //               } else {
-// //                 alert('You are absolutely correct! Well done!');
-// //                 break;
-// //             }
-// //         }  
-// //     }
-// //     alert('You have reached the maximum number of 4 incorrect attempts! Game Over!');    
-// // }
-
-// var reply = prompt('Toyota has several car models that I like. Can you name one of them?').toLowerCase();
-
-// // while ((reply !== options[0]) || (reply !== options[1]) || (reply !== options[2])) {
-// //     s--;
-// //     var answer = prompt('You have ' + n + ' more attempt(s) to get it correct! Guess again!');
-// // }
-
-// while ((reply !== options[i]) && (s > 1)) {
-//             s--;
-//             var ans = prompt('You have ' + n + ' more attempt(s) to get it correct! Guess again!');
-//             if (ans === options[i]) {
-//                 alert('Correct bro!');
-//             } else if (ans !== options[i]) {
-//                 alert('Wrong bro!');
-//             } else {
-//                 if (guess > num) {
-//                     alert('Whoa! Your guess was too high! I have not caught that many Pokémon!');
-//                   } else if (guess < num) {
-//                   alert('Whoa! Your guess was too low! I have caught more Pokémon than that!');
-//                   } else {
-//                     alert('You are absolutely correct! Well done!');
-//                     break;
-//                 }
-//             }  
-//         }
-
-// for (i=0; i < options.length; i++) {
-//     if (reply === options[i]) {
-//         alert('Correct');
-//         console.log(reply + ' was one of the correct answers.');
-//         break;
-//     } else {
-//         while ((s > 1) && (reply !== options[i])) {
-//             s--;
-//         alert('Wrong');
-//         var re_guess = prompt('Guess again!');
-//         console.log(reply + ' was not one of the correct answers.');
-//         console.log(s);
-//             if (re_guess === options[i]) {
-//             alert('You finally got it right!');
-//             } else {
-//             alert('You are still wrong!');
-//             }   
-//         }
-//     }
-// }
